1b3790bab12592331504dcef0f47efae5ae8787d
ashishIG <ashish.bansal@infogain.com>
2018-09-14 16:32:44 +0530

    changes for case 123

--- SampleGitDemo1/src/p1/MainClass.java
+++ SampleGitDemo1/src/p1/MainClass.java
@@ -5,6 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Git sample Demo");
+		
+		System.out.println("This is new updates for case 123");
 
 	}
 
