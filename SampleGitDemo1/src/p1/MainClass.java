1bd386f118cf5594c859401f810943ff8e4b4362
ashishIG <ashish.bansal@infogain.com>
2018-09-14 16:36:37 +0530

    Adding array and for loop

--- SampleGitDemo1/src/p1/MainClass.java
+++ SampleGitDemo1/src/p1/MainClass.java
@@ -7,6 +7,13 @@ public class MainClass {
 		System.out.println("This is Git sample Demo");
 		
 		System.out.println("This is new updates for case 123");
+		
+		String names[] = {"ramesh","rahul"};
+		
+		for(int i = 0;i<names.length;i++)
+		{
+			System.out.println(names[i]);
+		}
 
 	}
 
